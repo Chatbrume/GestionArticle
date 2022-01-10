8fba01f236355af9ec326c7dca44d7e17e12c4a2
Amélie NIOCHE <nioche.amelie@gmail.com>
2022-01-10 12:10:55 +0100

    Ajout du launcher

--- GestionArticle-Launcher/src/main/java/fr/ensup/gestionarticle/launcher/Launcher.java
+++ GestionArticle-Launcher/src/main/java/fr/ensup/gestionarticle/launcher/Launcher.java
@@ -1,4 +1,20 @@
 package fr.ensup.gestionarticle.launcher;
 
+import fr.ensup.gestionarticle.domaine.Article;
+import fr.ensup.gestionarticle.service.ArticleService;
+
 public class Launcher {
+    public static void main(String[] args) {
+        Article article1 = new Article("test", "10/01/2020", "test");
+
+        ArticleService articleService = new ArticleService();
+        articleService.creer(article1);
+
+        Article article2 = articleService.recuperation(2);
+        if (article2 != null) {
+            System.out.println("LAUNCHER: R�cuperation du compte r�ussie " + article2.toString());
+        } else {
+            System.out.println("LAUNCHER: R�cuperation du compte �chou�e");
+        }
+    }
 }
