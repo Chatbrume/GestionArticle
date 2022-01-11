1f7e1e1bf584cd5e04f2c2e0f70c070062dfb13a
Amélie NIOCHE <nioche.amelie@gmail.com>
2022-01-11 16:34:28 +0100

    Utilisation de HikariDataSource pour cree la connexion a la base de donnee

--- GestionArticle-Container/src/main/java/fr/ensup/gestionarticle/container/GestionObject.java
+++ GestionArticle-Container/src/main/java/fr/ensup/gestionarticle/container/GestionObject.java
@@ -1,18 +1,30 @@
 package fr.ensup.gestionarticle.container;
 
+import com.zaxxer.hikari.HikariDataSource;
 import fr.ensup.gestionarticle.dao.ArticleDao;
 import fr.ensup.gestionarticle.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 public class GestionObject
 {
-    @Bean(initMethod = "initialisation", destroyMethod = "destruction")
-    public ArticleService articleService() {
-        return new ArticleService(articleDao());
+    @Bean()
+    public HikariDataSource dataSourceSk() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://@localhost:3306/gestionarticle?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
     }
 
     @Bean(initMethod = "initialisation", destroyMethod = "destruction")
     public ArticleDao articleDao() {
-        return new ArticleDao();
+        return new ArticleDao(dataSourceSk());
+    }
+
+    @Bean(initMethod = "initialisation", destroyMethod = "destruction")
+    public ArticleService articleService() {
+        return new ArticleService(articleDao());
     }
 }
--- GestionArticle-Dao/src/main/java/fr/ensup/gestionarticle/dao/ArticleDao.java
+++ GestionArticle-Dao/src/main/java/fr/ensup/gestionarticle/dao/ArticleDao.java
@@ -4,6 +4,7 @@ import fr.ensup.gestionarticle.domaine.Article;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,9 +14,38 @@ public class ArticleDao implements IDao
     private static final Logger LOGGER = LogManager.getLogger(ArticleDao.class.getName());
     private Connection cn = null;
 
+    private DataSource dataSourceSk;
+
+    public ArticleDao(DataSource dataSourceSk) {
+        this.dataSourceSk = dataSourceSk;
+        try {
+            cn = this.dataSourceSk.getConnection();
+        } catch (SQLException sqle) {
+            LOGGER.error(sqle.getMessage());
+        } finally {
+            LOGGER.info("Connexion a la base de donnee reussi !");
+        }
+    }
+
+    @Override
+    public void initialisation()
+    {
+        LOGGER.info("Initialisation");
+    }
+
+    @Override
+    public void destruction() {
+        try {
+            if( cn != null ) cn.close();
+        } catch (SQLException sqle) {
+            LOGGER.error(sqle.getMessage());
+        } finally {
+            LOGGER.info("Connexion fermer avec succes !");
+        }
+    }
+
     public Article getById(int id) {
         LOGGER.info("récupération de l'article id=" + id);
-        if( cn == null ) initialisation();
 
         Statement st = null;
         ResultSet rs = null;
@@ -48,7 +78,6 @@ public class ArticleDao implements IDao
     public void create(Article article) {
         LOGGER.info("création de l'article " + article.toString());
         if( article == null ) return;
-        if( cn == null ) initialisation();
 
         PreparedStatement pstmt = null;
 
@@ -74,7 +103,6 @@ public class ArticleDao implements IDao
     public Article update(Article article) {
         LOGGER.info("mise à jour de l'article " + article.toString());
         if( article == null ) return null;
-        if( cn == null ) initialisation();
 
         Article prevArticle = getById(article.getId());
         boolean nameChange = ! stringAreEquals(article.getName(), prevArticle.getName());
@@ -114,7 +142,6 @@ public class ArticleDao implements IDao
     public void delete(Article article) {
         LOGGER.info("suppression de l'article " + article.toString());
         if( article == null || article.getId() == null ) return;
-        if( cn == null ) initialisation();
 
         Statement st = null;
         try {
@@ -134,7 +161,6 @@ public class ArticleDao implements IDao
 
     public List<Article> getAll() {
         LOGGER.info("récupération de tous les articles");
-        if( cn == null ) initialisation();
 
         List<Article> articles = new ArrayList<>();
         Statement st = null;
@@ -170,36 +196,5 @@ public class ArticleDao implements IDao
         if( st1 == null && st2 == null || (st1 != null && st2 != null && st1.equals(st2)) )
             return true;
         return false;
-    }
-
-    @Override
-    public void initialisation()
-    {
-        try {
-            String url = "jdbc:mysql://@localhost:3306/gestionarticle?serverTimezone=UTC";
-            String login = "root";
-            String passwd = "";
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            cn = DriverManager.getConnection(url, login, passwd);
-        } catch (ClassNotFoundException cnfe) {
-            LOGGER.error(cnfe.getMessage());
-        } catch (SQLException sqle) {
-            LOGGER.error(sqle.getMessage());
-        } finally {
-            LOGGER.info("Connexion a la base de donnee reussi !");
-        }
-    }
-
-    @Override
-    public void destruction() {
-        LOGGER.info("DAO: destruction spring");
-        try {
-            if( cn != null ) cn.close();
-        } catch (SQLException sqle) {
-            LOGGER.error(sqle.getMessage());
-        } finally {
-            LOGGER.info("Connexion fermer avec succes !");
-        }
     }
 }
